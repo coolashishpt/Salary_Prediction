8357de2a7ff3c9e4e13bbb9d4e8712657025f559
coolashishpt <40580290+coolashishpt@users.noreply.github.com>
2020-01-13 02:13:00 +0530

    Add files via upload

---
+++ Simple Linear Regression Salary Pred Model.py
@@ -0,0 +1,41 @@
+
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values # Dependent Variable/Target Values
+
+# Splitting the Dataset into Training Set and Test Set
+"""
+    from sklearn.cross_validation import train_test_split
+"""
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predicting the Test Set Results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training Set Results
+plt.scatter(X_train, Y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary Vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the Test Set Results
+plt.scatter(X_test, Y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary Vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
